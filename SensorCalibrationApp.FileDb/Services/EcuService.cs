11ccd15b2167ae5590105efb1fdb033c6b738e9a
igor <igor.kostovski98@gmail.com>
2020-04-15 23:25:12 +0200

    added ecu service

---
+++ SensorCalibrationApp.FileDb/Services/EcuService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SensorCalibrationApp.Domain.Models;
+using SensorCalibrationApp.Domain.Services;
+
+namespace SensorCalibrationApp.FileDb.Services
+{
+    public class EcuService : IEcuService
+    {
+        private readonly FileDatabase _db;
+
+        public EcuService(FileDatabase db)
+        {
+            _db = db;
+        }
+
+        public async Task<List<EcuModel>> GetAll()
+        {
+            await _db.Load();
+            return _db.Collection;
+        }
+    }
+}
