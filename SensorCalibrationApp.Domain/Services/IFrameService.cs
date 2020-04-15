8a87438502f7a06c3f937396f5ae9222fc4bf56e
igor <igor.kostovski98@gmail.com>
2020-04-15 21:53:09 +0200

    added interfaces for frame and ecu services

---
+++ SensorCalibrationApp.Domain/Services/IFrameService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using SensorCalibrationApp.Domain.Models;
+
+namespace SensorCalibrationApp.Domain.Services
+{
+    public interface IFrameService
+    {
+        Task Update(int newId, FrameModel model);
+    }
+}
