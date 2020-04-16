a5b062e252d9f790628f77da1ed764b72f948303
igor <igor.kostovski98@gmail.com>
2020-04-16 16:53:38 +0200

    added loading of ecus

--- SensorCalibrationApp/DeviceSelection/DeviceSelectionViewModel.cs
+++ SensorCalibrationApp/DeviceSelection/DeviceSelectionViewModel.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections.ObjectModel;
 using SensorCalibrationApp.Domain.Models;
+using SensorCalibrationApp.Domain.Services;
+using SensorCalibrationApp.FileDb;
+using SensorCalibrationApp.FileDb.Services;
 
 namespace SensorCalibrationApp.DeviceSelection
 {
     class DeviceSelectionViewModel : ViewModelBase
     {
         private ObservableCollection<EcuModel> _ecuModels;
+        private FileDatabase _db;
+        private IEcuService _ecuService;
+
+        public DeviceSelectionViewModel()
+        {
+            _db = new FileDatabase();
+            _ecuService = new EcuService(_db);
+        }
 
         private EcuModel _selectedEcu;
         public EcuModel SelectedEcu
@@ -40,6 +51,9 @@ namespace SensorCalibrationApp.DeviceSelection
             }
         }
 
-
+        public async void LoadECUs()
+        {
+            _ecuModels = new ObservableCollection<EcuModel>(await _ecuService.GetAll());
+        }
     }
 }
