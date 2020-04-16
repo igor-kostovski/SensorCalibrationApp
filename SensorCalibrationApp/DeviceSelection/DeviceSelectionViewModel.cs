dbc0d244ae2795aa574149898bf6ae88e4cfb268
igor <igor.kostovski98@gmail.com>
2020-04-16 16:37:24 +0200

    added properties in device selection view model

--- SensorCalibrationApp/DeviceSelection/DeviceSelectionViewModel.cs
+++ SensorCalibrationApp/DeviceSelection/DeviceSelectionViewModel.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using SensorCalibrationApp.Domain.Models;
 
 namespace SensorCalibrationApp.DeviceSelection
 {
     class DeviceSelectionViewModel : ViewModelBase
     {
+        private ObservableCollection<EcuModel> _ecuModels;
+
+        private EcuModel _selectedEcu;
+        public EcuModel SelectedEcu
+        {
+            get { return _selectedEcu; }
+            set
+            {
+                _selectedEcu = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private DeviceModel _selectedDevice;
+        public DeviceModel SelectedDevice
+        {
+            get { return _selectedDevice; }
+            set
+            {
+                _selectedDevice = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private FrameModel _selectedFrame;
+        public FrameModel SelectedFrame
+        {
+            get { return _selectedFrame; }
+            set
+            {
+                _selectedFrame = value;
+                OnPropertyChanged();
+            }
+        }
+
+
     }
 }
