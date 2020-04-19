8bd68be32e1b3118d4be79ec73361ee0404adda0
igor <igor.kostovski98@gmail.com>
2020-04-19 23:38:21 +0200

    added lin config class

---
+++ RimacLINBusInterfacesLib/LinConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using RimacLINBusInterfacesLib.Enums;
+
+namespace RimacLINBusInterfacesLib
+{
+    public class LinConfiguration
+    {
+        public BaudRate BaudRate { get; set; }
+        public HardwareMode HardwareMode { get; set; }
+    }
+}
