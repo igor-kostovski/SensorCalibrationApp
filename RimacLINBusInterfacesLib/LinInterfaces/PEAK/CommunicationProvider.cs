e363c8e9e7940c267432b7e2755cdcafffb1ccb9
igor <igor.kostovski98@gmail.com>
2020-04-19 23:27:01 +0200

    added peak classes

---
+++ RimacLINBusInterfacesLib/LinInterfaces/PEAK/CommunicationProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RimacLINBusInterfacesLib.LinInterfaces.PEAK
+{
+    internal class CommunicationProvider
+    {
+    }
+}
