fb2e33aa944f4ccef3955229f3daaba5fd4612d4
igor <igor.kostovski98@gmail.com>
2020-04-22 12:40:30 +0200

    implemented pt sensor and idevice interface

--- SensorCalibrationApp.Domain/Devices/PTSensor.cs
+++ SensorCalibrationApp.Domain/Devices/PTSensor.cs
@@ -1,8 +1,131 @@
-﻿using SensorCalibrationApp.Domain.Interfaces;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using SensorCalibrationApp.Domain.Interfaces;
 
 namespace SensorCalibrationApp.Domain.Devices
 {
     public class PTSensor : IDevice
     {
+        public bool HasError { get; set; }
+        public string Message { get; set; }
+
+        private List<PTSensorValue> Data { get; }
+
+        private Func<int, double> GetPress = x => x * 0.0088;
+        private Func<int, double> GetTemp = x => x * 0.1 - 40;
+
+        public PTSensor()
+        {
+            Data = new List<PTSensorValue>();
+            Message = "";
+            HasError = false;
+        }
+
+        public void ProcessData(byte[] data)
+        {
+            const int sizeOfEachValue = 12;
+            data = new[] { data[0], data[1], data[2], data[3], data[4] };
+
+            IEnumerable bits = new BitArray(data);
+            int numberOfValues = bits.OfType<bool>().Count() / sizeOfEachValue + 1;
+
+            ParseBits(numberOfValues, bits);
+            Validate();
+        }
+
+        private void Validate()
+        {
+            var error = Data.SingleOrDefault(x => x.Type == PTSensorData.Error);
+            var binary = error.Binary.ToList();
+
+            if (!IsValid(binary))
+                return;
+
+            Data.Remove(error);
+            ProcessValues();
+        }
+
+        private bool IsValid(List<int> binary)
+        {
+            if (binary.Contains(1))
+            {
+                Message = (
+                    (PTSensorError)binary.FindIndex(x => x == 1)
+                ).ToString();
+                HasError = true;
+
+                return false;
+            }
+
+            return true;
+        }
+
+        private void ProcessValues()
+        {
+            var messages = new List<string>();
+
+            Data.ForEach(x =>
+            {
+                var Get = x.Type == PTSensorData.Pressure ? GetPress : GetTemp;
+
+                var binaryStr = String.Concat(x.Binary);
+                var value = Convert.ToInt32(binaryStr, 2);
+
+                messages.Add($"{x.Type.ToString()}: {Get(value)}");
+            });
+
+            Message = string.Join("\n", messages);
+        }
+
+        private void ParseBits(int numberOfValues, IEnumerable bits)
+        {
+            for (int i = 0; i < numberOfValues; i++)
+            {
+                var binaryVal = bits.OfType<bool>()
+                    .Skip(i * 12)
+                    .Take(12)
+                    .Reverse()
+                    .Select(x => x ? 1 : 0);
+
+                Data.Add(new PTSensorValue
+                {
+                    Binary = binaryVal,
+                    Type = (PTSensorData) i
+                });
+            }
+        }
+
+        public void ClearData()
+        {
+            Message = "";
+            HasError = false;
+            Data.Clear();
+        }
+
+        #region Members
+        private class PTSensorValue
+        {
+            public PTSensorData Type { get; set; }
+            public IEnumerable<int> Binary { get; set; }
+        }
+
+        private enum PTSensorData
+        {
+            Pressure,
+            ExternalTemperature,
+            InternalTemperature,
+            Error
+        }
+
+        private enum PTSensorError
+        {
+            GlobalFault,
+            ChecksumError,
+            ResponseError,
+            TNCTOC
+        }
+        #endregion
     }
 }
--- SensorCalibrationApp.Domain/Interfaces/IDevice.cs
+++ SensorCalibrationApp.Domain/Interfaces/IDevice.cs
@@ -2,5 +2,10 @@
 {
     public interface IDevice
     {
+        bool HasError { get; set; }
+        string Message { get; set; }
+
+        void ProcessData(byte[] data);
+        void ClearData();
     }
 }
