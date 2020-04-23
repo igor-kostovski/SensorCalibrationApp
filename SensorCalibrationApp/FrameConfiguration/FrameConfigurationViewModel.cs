bedcf04dfd3a658b93759639bcc4b5992154b8c4
igor <igor.kostovski98@gmail.com>
2020-04-23 13:46:37 +0200

    added view model dependencies, set up view loading

--- SensorCalibrationApp/FrameConfiguration/FrameConfigurationViewModel.cs
+++ SensorCalibrationApp/FrameConfiguration/FrameConfigurationViewModel.cs
@@ -1,9 +1,39 @@
-﻿using SensorCalibrationApp.Domain.Models;
+﻿using RimacLINBusInterfacesLib.LinInterfaces.PEAK;
+using SensorCalibrationApp.Common;
+using SensorCalibrationApp.Common.Enums;
+using SensorCalibrationApp.Domain.Enums;
+using SensorCalibrationApp.Domain.Interfaces;
+using SensorCalibrationApp.Domain.Models;
+using SensorCalibrationApp.Domain.Services;
+using SensorCalibrationApp.Domain.Services.CommandService;
+using SensorCalibrationApp.FileDb;
+using SensorCalibrationApp.FileDb.Services;
 
 namespace SensorCalibrationApp.FrameConfiguration
 {
     class FrameConfigurationViewModel : ViewModelBase
     {
+        private readonly ICommandService _commandService;
+        private readonly FileDatabase _db;
+        private readonly IFrameService _frameService;
+        private readonly ILinProvider _linProvider;
+
+        public FrameConfigurationViewModel()
+        {
+            var linConfig = new LinConfiguration
+            {
+                BaudRate = BaudRate.Baud_192,
+                HardwareMode = HardwareMode.Master
+            };
+            _linProvider = new PeakLinInterface(linConfig);
+            _commandService = new CommandService(_linProvider);
+            _db = new FileDatabase();
+            _frameService = new FrameService(_db);
+        }
+
+        private DeviceType _frameDeviceType;
+        private int _frameDbId;
+
         private FrameModel _frame;
         public FrameModel Frame
         {
@@ -15,5 +45,16 @@ namespace SensorCalibrationApp.FrameConfiguration
             }
         }
 
+        public void Set(int id, DeviceType device)
+        {
+            _frameDbId = id;
+            _frameDeviceType = device;
+        }
+
+        public void LoadFrame()
+        {
+            _commandService.Load(_frameDeviceType);
+            //Frame = _commandService.Get(_frameDbId);
+        }
     }
 }
