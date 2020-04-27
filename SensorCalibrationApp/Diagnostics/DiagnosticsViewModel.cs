4c712ff465b1cf5efcd3eb9279adbbe782d2925e
igor <igor.kostovski98@gmail.com>
2020-04-27 10:28:06 +0200

    added diagnostics view markup and basic view model methods

--- SensorCalibrationApp/Diagnostics/DiagnosticsViewModel.cs
+++ SensorCalibrationApp/Diagnostics/DiagnosticsViewModel.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SensorCalibrationApp.Domain;
+using SensorCalibrationApp.Domain.Services.CommandService;
 
 namespace SensorCalibrationApp.Diagnostics
 {
     class DiagnosticsViewModel : ViewModelBase
     {
+        private readonly ICommandService _commandService;
+        private readonly EventManager _eventManager;
+
+        public DiagnosticsViewModel(ICommandService commandService, EventManager eventManager)
+        {
+            _commandService = commandService;
+            _eventManager = eventManager;
+        }
+
+        public void Load()
+        {
+            _eventManager.PushData += OnNewData;
+        }
+
+        public override void Unload()
+        {
+            _eventManager.PushData -= OnNewData;
+        }
+
+        private void OnNewData(object sender, string e)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
