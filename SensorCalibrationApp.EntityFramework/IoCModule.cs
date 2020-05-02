a160539b9cea75ca84bb3c184082dcc6ab20fd8e
igor <igor.kostovski98@gmail.com>
2020-05-02 19:32:34 +0200

    added ioc modules for file db and ef projects, updated autofac config file accordingly

---
+++ SensorCalibrationApp.EntityFramework/IoCModule.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Autofac;
+using AutoMapper;
+using SensorCalibrationApp.Domain.Services;
+using SensorCalibrationApp.EntityFramework.Data;
+using SensorCalibrationApp.EntityFramework.Services;
+using Module = Autofac.Module;
+
+namespace SensorCalibrationApp.EntityFramework
+{
+    public class IoCModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<EcuService>().As<IEcuService>().SingleInstance();
+            builder.RegisterType<FrameService>().As<IFrameService>().SingleInstance();
+            builder.RegisterType<DataContext>().AsSelf().SingleInstance();
+            builder.RegisterType<Seeder>().As<ISeeder>().SingleInstance();
+
+            RegisterAutomapper(builder);
+
+            base.Load(builder);
+        }
+
+        private void RegisterAutomapper(ContainerBuilder builder)
+        {
+            var profiles = GetAutomapperProfiles();
+            builder.Register(ctx => new MapperConfiguration(cfg =>
+            {
+                foreach (var profile in profiles)
+                {
+                    cfg.AddProfile(profile);
+                }
+            }));
+
+            builder.Register(ctx => ctx.Resolve<MapperConfiguration>().CreateMapper()).As<IMapper>().SingleInstance();
+        }
+
+        private IEnumerable<Profile> GetAutomapperProfiles()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            return assembly
+                .GetTypes()
+                .Where(x => x.Name.EndsWith("Profile"))
+                .Where(x => typeof(Profile).IsAssignableFrom(x) && x.IsPublic && !x.IsAbstract)
+                .Distinct()
+                .Select(x => (Profile)Activator.CreateInstance(x));
+        }
+    }
+}
---
+++ SensorCalibrationApp.FileDb/IoCModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using SensorCalibrationApp.Domain.Services;
+using SensorCalibrationApp.FileDb.Services;
+
+namespace SensorCalibrationApp.FileDb
+{
+    public class IoCModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<FrameService>().As<IFrameService>().SingleInstance();
+            builder.RegisterType<EcuService>().As<IEcuService>().SingleInstance();
+            builder.RegisterType<FileDatabase>().AsSelf().SingleInstance();
+            builder.RegisterType<FileDatabase.Seeder>().As<ISeeder>().SingleInstance();
+
+            base.Load(builder);
+        }
+    }
+}
