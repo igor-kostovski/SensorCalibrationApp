09e7457311fd4928ef09051a67295ef299750b03
igor <igor.kostovski98@gmail.com>
2020-05-02 12:02:23 +0200

    added ef package, data context class and dbsets

---
+++ SensorCalibrationApp.EntityFramework/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using SensorCalibrationApp.Domain.Models;
+
+namespace SensorCalibrationApp.EntityFramework
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base()
+        { }
+
+        public DbSet<EcuModel> Ecus { get; set; }
+        public DbSet<FrameModel> Frames { get; set; }
+        public DbSet<DeviceModel> Devices { get; set; }
+        public DbSet<SignalModel> Signals { get; set; }
+    }
+}
---
+++ SensorCalibrationApp.EntityFramework/EntityConfigurations/EcuConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using SensorCalibrationApp.Domain.Models;
+
+namespace SensorCalibrationApp.EntityFramework.EntityConfigurations
+{
+    public class EcuConfiguration : EntityTypeConfiguration<EcuModel>
+    {
+        public EcuConfiguration()
+        {
+            
+        }
+    }
+}
