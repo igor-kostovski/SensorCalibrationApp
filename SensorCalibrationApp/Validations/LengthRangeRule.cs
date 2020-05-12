914b99c5b369ab638793653dde524f1d426bacd7
igor <igor.kostovski98@gmail.com>
2020-05-12 11:25:16 +0200

    added validation rules for length and name props

---
+++ SensorCalibrationApp/Validations/LengthRangeRule.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace SensorCalibrationApp.Validations
+{
+    public class LengthRangeRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var length = (byte)GetValue(value);
+
+            if (length > 8 || length < 1)
+                return new ValidationResult(false,
+                    $"Please enter length in the range: { 1 } - { 8 }.");
+            return ValidationResult.ValidResult;
+        }
+
+        private object GetValue(object value)
+        {
+            if (value is BindingExpression binding)
+                return binding.ResolvedSource
+                    .GetType()
+                    .GetProperty(binding.ResolvedSourcePropertyName)?
+                    .GetValue(binding.ResolvedSource, null);
+
+            return value;
+        }
+    }
+}
---
+++ SensorCalibrationApp/Validations/RequiredRule.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace SensorCalibrationApp.Validations
+{
+    public class RequiredRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var field = (string)GetValue(value);
+
+            if (string.IsNullOrWhiteSpace(field))
+                return new ValidationResult(false,
+                    "This field is required.");
+            return ValidationResult.ValidResult;
+        }
+
+        private object GetValue(object value)
+        {
+            if (value is BindingExpression binding)
+                return binding.ResolvedSource
+                    .GetType()
+                    .GetProperty(binding.ResolvedSourcePropertyName)?
+                    .GetValue(binding.ResolvedSource, null);
+
+            return value;
+        }
+    }
+}
